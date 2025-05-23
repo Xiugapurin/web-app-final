7e8c5f9e1a3c5c2ccd96446a9e24c4ed0715b2a7
“Xiugapurin” <jason32567810@gmail.com>
2025-05-24 00:31:13 +0800

    update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col w-full justify-center items-center gap-8">
         <h2 class="text-5xl">Home Page</h2>
-        <p class="text-xl">This is home page. Implement join room & create room functionality.</p>
+        <p class="text-xl">This is home page.</p>
         <p class="text-xl">Implement join room & create room functionality.</p>
     </div>
 </template>
