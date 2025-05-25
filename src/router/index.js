3124a8b647f11b31af3afa933c8cde262b2742cd
ac12341234 <apple72601@gmail.com>
2025-05-25 17:47:22 +0800

    rooms view

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RoomsView from "../views/RoomsView.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomeView,
+  },
+  {
+    path: "/rooms",
+    name: "Rooms",
+    component: RoomsView
   },
   {
     path: "/room/:roomId",
---
+++ src/views/RoomsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col w-full justify-center items-center gap-8">
+    <p>rooms</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const roomIdInput = ref("");
+const router = useRouter();
+</script>
