1b3aae267026413a012c3bc4fd1fdd4b5091a805
ac12341234 <apple72601@gmail.com>
2025-05-25 16:37:08 +0800

    center area

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="flex flex-col w-full justify-center items-center gap-8">
-        <h2 class="text-5xl">Home Page</h2>
+        <!-- <h2 class="text-5xl">Home Page</h2>
         <p class="text-xl">This is home page.</p>
-        <p class="text-xl">Implement join room & create room functionality.</p>
+        <p class="text-xl">Implement join room & create room functionality.</p> -->
+        <div class="flex mt-16 w-[60%] h-[60vh] bg-white rounded-xl overflow-hidden items-center">
+            <!-- 左侧区域 -->
+            <div class="flex-1 "></div>
+            
+            <!-- 灰色分隔线（垂直居中且高度80%） -->
+            <div class="w-1 bg-gray-300 h-[60%]"></div>
+            
+            <!-- 右侧区域 -->
+            <div class="flex-1"></div>
+        </div>
     </div>
 </template>
 
