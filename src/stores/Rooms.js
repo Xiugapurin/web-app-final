8e758814de7445c2b4c642e26750dff3efc61664
ac12341234 <apple72601@gmail.com>
2025-05-28 14:44:28 +0800

    Update Rooms.js

--- src/stores/Rooms.js
+++ src/stores/Rooms.js
@@ -3,12 +3,13 @@ import { defineStore } from 'pinia'
 
 export const useRoomsStore = defineStore('rooms', () => {
   const roomList = ref([])
+  
   function addRoom(room) {
     roomList.value.push(room)
   }
 
-  function removeRoom(room) {
-    roomList.value.pop(room)
+  function removeRoom(roomId) { 
+    roomList.value = roomList.value.filter(room => room.id !== roomId)
   }
 
   return { roomList, addRoom, removeRoom }
